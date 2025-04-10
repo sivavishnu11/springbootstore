2d07198eb6c64855dd95868fe3ace8d77608ef62
sivavishnu11 <122550313+sivavishnu11@users.noreply.github.com>
2025-04-10 17:26:47 -0500

    added debug

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -14,6 +14,7 @@ public class StoreApplication {
         //OrderService orderService = new OrderService();
         //orderService.setPaymentService(new PaypalPaymentService());
         orderService.makeOrder();
+        System.out.println("Order made");
     }
 
 }
